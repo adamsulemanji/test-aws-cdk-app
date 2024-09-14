565d823dff6e9258c42bd4b06026e3a543ebb652
adamsulemanji <84648379+adamsulemanji@users.noreply.github.com>
2024-09-14 17:34:25 -0500

    Updating lambda integration

--- lib/apigateway.ts
+++ lib/apigateway.ts
@@ -4,30 +4,36 @@ import * as cdk from 'aws-cdk-lib';
 import * as apig from 'aws-cdk-lib/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 
-
 export class ApiGateway extends Construct {
-    constructor(scope: Construct, id: string) {
-        super(scope, id);
-    
-        const base = new apig.RestApi(this, 'TestAwsCdkAppApiGateway', {
-            deployOptions: {
-                stageName: 'test'
-            }
-        });
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
 
-        const resource = base.root.addResource('test');
+    const base = new apig.RestApi(this, 'TestAwsCdkAppApiGateway', {
+    });
 
-        const test1Lambda = new lambda.Function(this, 'Test1Lambda', {
-            runtime: lambda.Runtime.NODEJS_20_X,
-            handler: 'test1_GET.handler',
-            code: lambda.Code.fromAsset('lib/lambdas')
-        });
+    const resource = base.root.addResource('test');
+    const test1Lambda = new lambda.Function(this, 'Test1Lambda', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'test1_GET.handler',
+      code: lambda.Code.fromAsset('lib/lambdas')
+    });
+    const test1Integration = new apig.LambdaIntegration(test1Lambda);
+    resource.addMethod('GET', test1Integration);
 
-        const test1Integration = new apig.LambdaIntegration(test1Lambda);
-        resource.addMethod('GET', test1Integration);
+    const resource2 = base.root.addResource('test2');
 
-        const resource2 = base.root.addResource('test2');
-        resource2.addMethod('GET');
-        resource2.addMethod('POST');
-    }
+    // Create a new Lambda function for test2
+    const test2Lambda = new lambda.Function(this, 'Test2Lambda', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'test2.handler',
+      code: lambda.Code.fromAsset('lib/lambdas')
+    });
+
+    // Create a Lambda integration for test2
+    const test2Integration = new apig.LambdaIntegration(test2Lambda);
+
+    // Add the Lambda integration to the GET and POST methods
+    resource2.addMethod('GET', test2Integration);
+    resource2.addMethod('POST', test2Integration);
+  }
 }
