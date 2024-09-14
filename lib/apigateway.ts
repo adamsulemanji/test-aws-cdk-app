f5589cdd2386c5633666878b24db4ed1dce5de0c
adamsulemanji <84648379+adamsulemanji@users.noreply.github.com>
2024-09-14 16:52:17 -0500

    Adding 2 new apigateway resource routes

--- lib/apigateway.ts
+++ lib/apigateway.ts
@@ -8,10 +8,18 @@ export class ApiGateway extends Construct {
     constructor(scope: Construct, id: string) {
         super(scope, id);
     
-        new apig.RestApi(this, 'TestAwsCdkAppApiGateway', {
+        const base = new apig.RestApi(this, 'TestAwsCdkAppApiGateway', {
             deployOptions: {
                 stageName: 'test'
             }
         });
+
+        const resource = base.root.addResource('test');
+        resource.addMethod('GET');
+
+
+        const resource2 = base.root.addResource('test2');
+        resource2.addMethod('GET');
+        resource2.addMethod('POST');
     }
 }
