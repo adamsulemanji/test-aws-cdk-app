81a27802342e08ed6e5d7d4c73853393e017eef3
adamsulemanji <84648379+adamsulemanji@users.noreply.github.com>
2024-09-14 14:35:32 -0500

    Adding a test SQS DLQ

--- lib/test-aws-cdk-app-stack.ts
+++ lib/test-aws-cdk-app-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
 import { Construct } from 'constructs';
 
@@ -10,6 +11,26 @@ export class TestAwsCdkAppStack extends cdk.Stack {
 
     new Queue(this, 'TestAwsCdkAppQueue', {
       visibilityTimeout: cdk.Duration.seconds(300)
+    });
+
+    new Queue(this, 'TestAwsCdkAppDeadLetterQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
+
+
+    // Define the pipeline
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'CDKPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('adamsulemanji/test-aws-cdk-app', 'master', {
+          authentication: cdk.SecretValue.secretsManager("github_token2")
+        }),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth'
+        ]
+      }),
     });
 
   }
