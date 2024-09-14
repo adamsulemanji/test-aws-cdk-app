9b5d1639601223a1924496393ddbe22d085943bb
adamsulemanji <84648379+adamsulemanji@users.noreply.github.com>
2024-09-14 12:19:47 -0500

    Initial commit of test aws cdk

--- lib/test-aws-cdk-app-stack.ts
+++ lib/test-aws-cdk-app-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TestAwsCdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TestAwsCdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Queue(this, 'TestAwsCdkAppQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
+
   }
 }
