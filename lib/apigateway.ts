4d1475b6be2af491a808ba9d15b1d5dc69920724
adamsulemanji <84648379+adamsulemanji@users.noreply.github.com>
2024-09-14 17:58:55 -0500

    Moving lambda resources

--- lib/apigateway.ts
+++ lib/apigateway.ts
@@ -1,6 +1,5 @@
 import { Construct } from 'constructs';
 
-import * as cdk from 'aws-cdk-lib';
 import * as apig from 'aws-cdk-lib/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 
@@ -9,31 +8,17 @@ export class ApiGateway extends Construct {
     super(scope, id);
 
     const base = new apig.RestApi(this, 'TestAwsCdkAppApiGateway', {
+        restApiName: 'TestAwsCdkAppApiGateway',
+        description: 'This is a test API Gateway',
     });
 
     const resource = base.root.addResource('test');
-    const test1Lambda = new lambda.Function(this, 'Test1Lambda', {
+    const testLambda = new lambda.Function(this, 'TestLambda', {
       runtime: lambda.Runtime.NODEJS_20_X,
       handler: 'test1_GET.handler',
-      code: lambda.Code.fromAsset('lib/lambdas')
+      code: lambda.Code.fromAsset('lambdas')
     });
-    const test1Integration = new apig.LambdaIntegration(test1Lambda);
-    resource.addMethod('GET', test1Integration);
-
-    const resource2 = base.root.addResource('test2');
-
-    // Create a new Lambda function for test2
-    const test2Lambda = new lambda.Function(this, 'Test2Lambda', {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      handler: 'test2.handler',
-      code: lambda.Code.fromAsset('lib/lambdas')
-    });
-
-    // Create a Lambda integration for test2
-    const test2Integration = new apig.LambdaIntegration(test2Lambda);
-
-    // Add the Lambda integration to the GET and POST methods
-    resource2.addMethod('GET', test2Integration);
-    resource2.addMethod('POST', test2Integration);
+    const testIntegration = new apig.LambdaIntegration(testLambda);
+    resource.addMethod('GET', testIntegration);
   }
 }
