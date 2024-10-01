0b20fcebc4ede00c9a0d21ecfc1356026f2bc262
adamsulemanji <84648379+adamsulemanji@users.noreply.github.com>
2024-10-01 17:08:59 -0500

    Testing deployment

--- lib/sqs.ts
+++ lib/sqs.ts
@@ -26,5 +26,9 @@ export class SQSConstruct extends Construct {
     new sqs.Queue(this, 'TestAwsCdkAppQueue3', {
       visibilityTimeout: cdk.Duration.seconds(300),
     });
+
+    new sqs.Queue(this, 'TestAwsCdkAppQueue4', {
+      visibilityTimeout: cdk.Duration.seconds(300),
+    });
   }
 }
